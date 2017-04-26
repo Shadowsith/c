ce9acf895a2d8345500235baad3bf9c7c4577761
Shadowsith <shadowsith94@gmail.com>
2017-04-26 23:58:16 +0200

    Struct

--- structs/struct.c
+++ structs/struct.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct adresse{
     char strasse[20];
@@ -13,10 +14,22 @@ struct student{
     char nachname[20];
     char vorname[20];
     struct adresse wohnort;
-}
+};
 
 
 int main(void){
 
+    struct adresse firstadress;
+    strcpy(firstadress.strasse,"Am Graben 2");
+    strcpy(firstadress.hausnummer,"2A");
+    firstadress.postleitzahl = 85110;
+    strcpy(firstadress.stadt, "Kipfenberg");
+
+    struct student firststudent;
+    strcpy(firststudent.matrikelnummer,"A5X0");
+    strcpy(firststudent.nachname,"Mayer");
+    strcpy(firsstudent.vorname,"Philip");
+    
+    
 
 }
